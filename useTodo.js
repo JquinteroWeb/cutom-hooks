78c96da09c7bb1d95afccd51dc1ca080709c607c
JQuintero Web <102717524+JquinteroWeb@users.noreply.github.com>
2023-02-19 23:57:16 -0500

    Primer Commit

---
+++ useTodo.js
@@ -0,0 +1,50 @@
+
+import { useEffect, useReducer } from 'react';
+import { todoReducer } from '../08-useReducer/todoReducer';
+
+const initialState = [];
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todo') || []);
+}
+export const useTodo = () => {
+
+    const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+        localStorage.setItem('todo', JSON.stringify(todos) || []);
+    }, [todos]);
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: 'Add Todo',
+            payload: todo
+        }
+        dispatchTodo(action);
+    }
+
+    const handleDeleteTodo = (id) => {
+        const action = {
+            type: 'Delete Todo',
+            payload: id
+        }
+        dispatchTodo(action)
+    };
+    const handleToggleTodo = (id) => {
+        console.log(id);
+        const action = {
+            type: 'Toggle Todo',
+            payload: id
+        }
+        dispatchTodo(action)
+    };
+    return {
+        todos,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+        todoCount:todos.length,
+        pendingTodoCount:todos.filter(todo => !todo.done).length
+    }
+}
+
